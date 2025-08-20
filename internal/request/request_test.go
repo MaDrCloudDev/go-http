b55d1a6a6bee62f934265a50edbc84a4e6c6fa3c
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2025-08-19 19:07:59 -0700

    testing and errors
    testing and errors

--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -1,11 +1,32 @@
 package request
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRequestLineParse(t *testing.T) {
-	assert.Equal(t, "test", "test")
+	// Test: good GET request line
+	r, err := RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\n\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Test: good GET request line with path
+	r, err = RequestFromReader(strings.NewReader("GET /test HTTP/1.1\r\nHost: localhost:42069\r\n\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/test", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Test: invalid number of parts in request line
+	_, err = RequestFromReader(strings.NewReader("/test HTTP/1.1\r\nHost: localhost:42069\r\n\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+	require.ErrorIs(t, err, ErrMalformedRequestLine)
 }
